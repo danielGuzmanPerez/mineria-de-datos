a79132e62d8544b6ebff97824d44169819a2def7
Victor Daniel Guzman Perez <vdgp_12@hotmail.com>
2021-10-12 20:29:56 -0500

    proyecto terminado

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import math
+# import math
 import sys, os
 
-from pandas import *
+# from pandas import *
 import pandas as pd
 from numpy import *
 import numpy as np
 import random
-import re
-from sys import *
+
+# import re
+# from sys import *
 
 # ubicacion = input(str("Hola para leer tu archivo indicame la direccion y el archivo como tal y su extension: "))
+# global Column_Target
+Column_Target = 0
+# global TargetType
+TargetType = 0
 
 df = pd.DataFrame(pd.read_excel("./reviews_sentiment.xlsx", sheet_name=0))
 
@@ -30,20 +35,16 @@ df = pd.DataFrame(pd.read_excel("./reviews_sentiment.xlsx", sheet_name=0))
     print("La cantidad de Filas del Dataframe es: ", df.shape[0])
     print("Cantidad de filas para Entrenamiento:  ", int(CountTrainer))
     print("Cantidad de filas para Pruebas:  ", int(CountTest))
-
     # CANTIDAD DE VECES QUE SE VA ITERAR EL ZERO-R
     for a in range(iter):
-
         print("\n \n \n _____________________Iteración numero ", str(a + 1), "_____________________")
         # SE CREA LA LISTA DE ENTRENAMIENTO CON NUMEROS RANDOM
         ListTrainer = random.sample(range(int(MaxRows)), int(CountTrainer))
         ListTest = range(int(MaxRows))
         ListTest = list(ListTest)
-
         # SE CREA UNA LISTA CON TODOS LOS VALORES Y SE ELIMINAN LOS DE LISTA DE ENTRENAMIENTO
         for x in ListTrainer:
             ListTest.remove(x)
-
         # CREAMOS DATAFRAME EN BASE EL INDICE DE LISTA DE ENTRENAMIENTO
         DataFrameTrainer = df.loc[ListTrainer]
         # CREAMOS DATAFRAME EN BASE EL INDICE DE LA LISTA DE PRUEBAS
@@ -59,19 +60,13 @@ df = pd.DataFrame(pd.read_excel("./reviews_sentiment.xlsx", sheet_name=0))
         # EN ESTE APARTADO IMPRIMIMOS LA CLASE QUE MAS SE REPITE LA CANTIDAD DE VECES QUE SE ENCUENTRA TANTO EN EL DATAFRAME DE PRUEBA Y ENTRENAMIENTO
         DataFrameTrainerNumeric =  DataFrameTrainer.select_dtypes(include=['int64', 'float64'])
         DataFrameTrainerString = DataFrameTrainer.select_dtypes(include=['object', 'bool'])
-
         columns_numeric = list(DataFrameTrainerNumeric.columns.values)
         columns_string = list(DataFrameTrainerString.columns.values)
-
         # AGREGANDO ETIQUETAS DE LOS ATRIBUTOS
         for a in columns_string:
             ListAttributeString.append(DataFrameTrainerString[a].unique().tolist())
-
         # SE CONSTRUYE LAS TABLAS DE FRECUENCIA
-
-
         sys.exit()
-
     #os.system("PAUSE")
     #print("\n" * 100)'''
 
@@ -104,8 +99,7 @@ def DividirDataFrame(df):
     return DataFrameTrainer, DataFrameTest
 
 
-def SepararStringConNumerico(DataFrameTrainer,
-                             DataFrameTest):  # Divide los datos del dataframe segun el tipo de dato que sean
+def SepararStringConNumerico(DataFrameTrainer):  # Divide los datos del dataframe segun el tipo de dato que sean
     columns_numeric_Name = list((DataFrameTrainer.select_dtypes(include=['int64', 'float64'])).columns.values)
     columns_string_Name = list((DataFrameTrainer.select_dtypes(include=['object', 'bool'])).columns.values)
     return columns_numeric_Name, columns_string_Name
@@ -118,10 +112,16 @@ def DefinirTipoDeDato(HeaderString, HeaderNumeric):
     return TipesString, TipesNumeric
 
 
-def CalcularDistanciaEuclidiana(DataFrameTest, DataFrameTrainer, HeaderNamesNumeric):
+def CalcularDistanciaEuclidiana(DataFrameTest, DataFrameTrainer, HeaderNamesNumeric, Column_Target, TargetType):
     suma = 0
     ListaSuma = []
     ListaEuclidiana = []
+
+    # SE ELIMINA LA COLUMNA TARGET PARA NO COMPARARLA
+    if TargetType == "regresion":
+        DataFrameTest.drop([Column_Target], axis=1)
+        DataFrameTrainer.drop([Column_Target], axis=1)
+        HeaderNamesNumeric.remove(Column_Target)
 
     for test in range(len(DataFrameTest)):  # Itera cada una de las FILAS del dataframe test
         ListaSuma = []
@@ -138,23 +138,22 @@ def CalcularDistanciaEuclidiana(DataFrameTest, DataFrameTrainer, HeaderNamesNume
 
 def SumarDIstancias(ListaHamming, ListaEuclidiana):
     suma = []
-    print(ListaHamming)  # SE ITERAN LAS LISTAN PARA SUMAR SU VALOR
     # NO IMPORTA QUE LISTA SE ITERE, AMBAS CONTIENEN LA MISMA CANTIDAD
     for lista in range(len(ListaHamming)):
         for posicion in range(len(ListaHamming[lista])):
-            print(lista)
             ListaHamming[lista][posicion] = ListaHamming[lista][posicion] + ListaEuclidiana[lista][posicion]
+
     return ListaHamming
 
 
-def CalcularDistanciaHamming(DataFrameTest, DataFrameTrainer, HeaderNamesString):
+def CalcularDistanciaHamming(DataFrameTest, DataFrameTrainer, HeaderNamesString, Column_Target, TargetType):
     suma = 0
     ListaSuma = []
     ListaHamming = []
-
     if TargetType == "clasificacion":  # SE ELIMINA LA COLUMNA TARGET PARA NO COMPARARLA
         DataFrameTest.drop([Column_Target], axis=1)
         DataFrameTrainer.drop([Column_Target], axis=1)
+        HeaderNamesString.remove(Column_Target)
 
     for test in range(len(DataFrameTest)):  # Itera cada una de las FILAS del dataframe test
         ListaSuma = []
@@ -215,17 +214,66 @@ def Normalizar(DataFrameTrainer, DataFramePrueba, HeaderNamesString, HeaderNames
         b[Column_Target] = DataFrameTrainer[Column_Target]
         c[Column_Target] = DataFramePrueba[Column_Target]
 
-    return b, c
+    return b, c, Column_Target, TargetType
+
+    #SE OBTIENEN LOS K VALORES MAS CERCANOS  Y SU PRPOMEDIO O LA CLASE QUE MÁS SE REPITE
+    #SE OBTIENE EL ERROR CUADRATICO O PORCENTAJE DE ACIERTOS
+def Resultado( K, DataFrameTest, DataFrameTrainer,ListaSuma,TargetType,Column_Target):
+    ContadorCategorico = 0
+    ListaPromedio = []
+
+    for a in range(int(len(DataFrameTest.index.values))):
+        DataFrameTrainer.insert(int(len(DataFrameTrainer.columns.values)), "Distancia Con Valor de Prueba " + str(a),
+                                ListaSuma[a])
+        #SE IMPRIME EL DATAFRAME CON LAS COLUMNAS DE DISTANCIAS
+        print("\n_____________________________________________________________ Datos de prueba " + str(a) + ":\n ",
+              DataFrameTest.iloc[a])
+        # SE OBTIENEN LAS FILAS CON LAS K DISTANCIAS MÁS PEQUEÑAS
+        Temp = DataFrameTrainer.nsmallest(K, "Distancia Con Valor de Prueba " + str(a))
+
+        # EN CASO QUE EL TARGET SEA CATEGORICO
+        if TargetType == "clasificacion":
+            # SE OBTIENE LA EL VALOR DE LA COLUMNA QUE MAS VECES SE REPITE
+            Valor1 = Temp[Column_Target].value_counts().idxmax()
+            # SE OBTIENE EL VALOR ORIGINAL
+            Valor2 = DataFrameTest[Column_Target].iloc[a]
+            print("\nValor predecido: ", Valor1)
+            print("\nValor real: ", Valor2)
+            if Valor1 == Valor2:
+                print("\nPrediccion correcta")
+                ContadorCategorico += 1
+            else:
+                print("\nPrediccion incorrecta")
+
+        # EN CASO QUE EL TARGET SEA NUMERICO
+        elif TargetType == "regresion":
+            # SE OBTIENE EL PROMEDIO DE LA COLUMNA TARGET DE LOS K VALORES
+            promedio = Temp[Column_Target].mean()
+            ListaPromedio.append(promedio)
+            print("\n El Promedio es: ", promedio)
+            print("Valor real: ", DataFrameTest[Column_Target].iloc[a])
+
+    #SE MUESTRAN LOS RESULTADOS
+    if TargetType == "clasificacion":
+        print("\nPorcentaje de aciertos: ", (ContadorCategorico / len(DataFrameTest.index)) * 100)
+    elif TargetType == "regresion":
+       print("\nError Cuadratico medio para este conjunto de datos: ",(np.square(DataFrameTest[Column_Target].values- ListaPromedio)).mean())
+
+    print("\n ---------------------------------------------------Distancias---------------------------------------- ")
+    print(DataFrameTrainer.to_string())
+
 
 
 def KNN():
+    ListaPromedio = []
+    ContadorCategorico = 0
     ListaHamming = []
     ListaEuclidiana = []
     ListaSuma = []
     DataFrames = DividirDataFrame(df)  # SE RECIBEN LOS DATAFRAME DE DIVIDIRDATAFRAME() Y SE DIVIDEN
-    DataFrameTrainer = DataFrames[0]
-    DataFrameTest = DataFrames[1]
-    HeaderNames = SepararStringConNumerico(DataFrameTrainer, DataFrameTest)
+    DataFrameTrainer = pd.DataFrame(DataFrames[0])
+    DataFrameTest = pd.DataFrame(DataFrames[1])
+    HeaderNames = SepararStringConNumerico(DataFrameTrainer)
     HeaderNamesNumeric = HeaderNames[0]  # CONTIENE EL NOMBRE DE LAS COLUMNAS QUE SON NUMERICAS
     HeaderNamesString = HeaderNames[1]  # CONTIENE EL NOMBRE DE LAS COLUMNAS QUE SON CATEGORICAS
     Types = DefinirTipoDeDato(HeaderNamesString, HeaderNamesNumeric)
@@ -233,34 +281,41 @@ def KNN():
     TipesNumeric = Types[1]
 
     # IMPRIMIMOS LOS DATAFRAME
-    print("\n------------Datos de Entrenamiento------------")
+    print("\n------------------------------------Datos de Entrenamiento------------------------------------")
     print(DataFrameTrainer.head(5).to_string())
     print("\n", DataFrameTrainer.describe())
-    print("\n------------Datos de Prueba------------")
+    print("\n------------------------------------Datos de Prueba------------------------------------")
     print(DataFrameTest.head(5).to_string())
     if len(HeaderNamesNumeric) > 0:  # SE NORMALIZAN LOS DATAFRAMES SOLO SI CONTIENE VALORES NUMERICOS
         DataFrameTemp = Normalizar(DataFrameTrainer, DataFrameTest, HeaderNamesString, HeaderNamesNumeric, HeaderNames)
         DataFrameTrainerNorm = DataFrameTemp[0]
         DataFrameTestNorm = DataFrameTemp[1]
-        print("\n------------DATOS NORMALIZADOS--------------")
+        Column_Target = DataFrameTemp[2]
+        TargetType = DataFrameTemp[3]
+        print("\n------------------------------------DATOS NORMALIZADOS--------------------------------------")
         DataFrameTrainer[list(DataFrameTrainerNorm.columns.values)] = DataFrameTrainerNorm[
             list(DataFrameTrainerNorm.columns.values)]
         DataFrameTest[list(DataFrameTestNorm.columns.values)] = DataFrameTestNorm[
             list(DataFrameTestNorm.columns.values)]
         print(DataFrameTrainer.head(5).to_string())
 
-    K = int(input("Ingrese el valor de K "))
+    K = int(input("\nIngrese el valor de K "))
     # SE OBTIENEN LAS DISTANCIAS
+    # EN CASO DE QUE EL DATAFRAME CONTENGA VALORES CATEGORICOS
     if (len(HeaderNamesString) > 0):
-        ListaHamming = CalcularDistanciaHamming(DataFrameTest, DataFrameTrainer, HeaderNamesString)
+        ListaHamming = CalcularDistanciaHamming(DataFrameTest, DataFrameTrainer, HeaderNamesString, Column_Target,
+                                                TargetType)
+
+    # EN CASO DE QUE EL DATAFRAME CONTENGA VALORES NUMERICOS
     if len(HeaderNamesNumeric) > 0:
-        ListaEuclidiana = CalcularDistanciaEuclidiana(DataFrameTest, DataFrameTrainer, HeaderNamesNumeric)
+        ListaEuclidiana = CalcularDistanciaEuclidiana(DataFrameTest, DataFrameTrainer, HeaderNamesNumeric,
+                                                      Column_Target, TargetType)
 
         # SE SUMAN LAS DISTANCIAS EN CASO DE EXISTIR VALORES MIXTOS
     if len(HeaderNamesString) > 0 and len(HeaderNamesNumeric) > 0:
         ListaSuma = SumarDIstancias(ListaHamming, ListaEuclidiana)
 
-        #EN CASO QUE TODAS LAS COLUMNAS SEAN CATEGORICAS
+        # EN CASO QUE TODAS LAS COLUMNAS SEAN CATEGORICAS
     elif len(HeaderNamesString) > 0:
         ListaSuma = ListaHamming
 
@@ -268,23 +323,9 @@ def KNN():
     elif len(HeaderNamesNumeric) > 0:
         ListaSuma = ListaEuclidiana
 
+    Resultado(K,DataFrameTest, DataFrameTrainer, ListaSuma,TargetType,Column_Target)
     sys.exit()
 
-
-''' for a in DataFrameTestList:                         #COMPARANDO PRIMERO LOS CATEGORICOS
-        for b in DataFrameTrainerList:
-            for ValoresTest in a:
-                for ValoresTrainer in b:
-                    print("\n",ValoresTrainer,ValoresTest)
-                    if ValoresTest != ValoresTrainer:
-                        suma+=1
-
-            print(suma)
-            sys.exit()'''
-
-# print(minmax_norm(DataFrameTrainer[n]))
-Column_Target = 0
-TargetType = 0
 
 while True:
     print("1) KNN")
